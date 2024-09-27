4593ae9bbcafd4b0fab10751ed6f16abe28802ff
anthonycroft <tony@vbasolutions.co.uk>
2024-09-27 10:46:14 +0100

    completed rest apis

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-stores = [{"name": "My Store", "items": [{"name": "my item", "price": 15.99}]}]
+stores = [{"name": "My Store", "items": [{"name": "Chair", "price": 15.99}]}]
 
 
 @app.get("/store")
